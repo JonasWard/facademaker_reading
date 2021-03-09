ad89c39d43fd4acc78d9e5d0d945ed2ad246cfb9
Jonas Ward <ward@ugd.ai>
2021-03-09 23:35:22 +0100

    pattern generation implemented

--- src/pattern_generator.py
+++ src/pattern_generator.py
@@ -11,6 +11,13 @@ p_b_set={
     "easyFix_B" : [["easyfix_pos", "easyfix_neg"], ["easyfix_neg", "easyfix_pos"]]
 }
 
+inverted_patterns=[
+    "alternating_A",
+    "alternating_B",
+    "easyFix_A",
+    "easyFix_B"
+]
+
 def invert_pattern(value):
     if value=="positive":
         return "negative"
@@ -21,12 +28,15 @@ def invert_pattern(value):
     elif value=="easyfix_neg":
         return "easyfix_pos"
 
+def is_inverted(p_name):
+
+
 def filling_pattern(p_name, cnt = 4, fix = True):
     global p_b_set
 
     full_pattern_list=[p_b_set[p_name][i%len(p_b_set[p_name])][:] for i in range(cnt)]
 
-    if (p_name=="alternating_A" or p_name=="alternating_B") and (cnt%2==1) and fix:
+    if (p_name in inverted_patterns) and fix:
         full_pattern_list[0][0]=full_pattern_list[-1][-1]
 
     return full_pattern_list
@@ -38,7 +48,7 @@ def pyramid_pattern_parser(name="flat", cnt=4):
     global p_b_set
 
     pentagon_set=filling_pattern(name, cnt-1, fix=False)
-    if (name=="alternating_A" or name=="alternating_B"):
+    if (p_name in inverted_patterns):
         triangle_set=[[invert_pattern(v) for v in ([pentagon_set[0][0], pentagon_set[-1][-1]])]]
     else:
         triangle_set=[[pentagon_set[0][0], pentagon_set[-1][-1]]]
