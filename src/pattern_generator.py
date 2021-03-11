c726e80e0afc4b4d46c9aff7f8dc20d2f3425e53
Jonas Ward <ward@ugd.ai>
2021-03-11 13:21:33 +0100

    updated_pattern_generator

--- src/pattern_generator.py
+++ src/pattern_generator.py
@@ -7,13 +7,18 @@ p_b_set={
     "alternating_A" : [["negative", "positive"], ["positive", "negative"]],
     "alternating_B" : [["positive", "negative"], ["negative", "positive"]],
     "flaps" : ["flap"],
-    "easyFix_A" : [["easyfix_pos", "easyfix_pos"]],
-    "easyFix_B" : [["easyfix_neg", "easyfix_neg"]]
+    "easyFix_A" : [["easyfix_pos", "easyfix_pos"],["easyfix_pos_simple", "easyfix_pos_simple"]],
+    "easyFix_B" : [["easyfix_neg", "easyfix_neg"],["easyfix_neg_simple", "easyfix_neg_simple"]]
 }
 
 inverted_patterns=[
     "alternating_A",
     "alternating_B",
+    "easyFix_A",
+    "easyFix_B"
+]
+
+easy_fix_patterns=[
     "easyFix_A",
     "easyFix_B"
 ]
@@ -27,11 +32,27 @@ def invert_pattern(value):
         return "easyfix_neg"
     elif value=="easyfix_neg":
         return "easyfix_pos"
+    elif value=="easyfix_pos_simple":
+        return "easyfix_neg_simple"
+    elif value=="easyfix_neg_simple":
+        return "easyfix_pos_simple"
+
+def easy_fix_function(p_name, cnt = 4):
+    full_pattern_list=[p_b_set[p_name][1][:] for i in range(cnt)]
+    full_pattern_list[0][0]=p_b_set[p_name][0][0]
+    full_pattern_list[-1][-1]=p_b_set[p_name][0][1]
+
+    return full_pattern_list
 
 def filling_pattern(p_name, cnt = 4, fix = True):
     global p_b_set
 
-    full_pattern_list=[p_b_set[p_name][i%len(p_b_set[p_name])][:] for i in range(cnt)]
+    if p_name in easy_fix_patterns:
+        print("creating an easyFix patterns")
+        full_pattern_list=easy_fix_function(p_name, cnt=cnt)
+    else:
+        print("creating a standard pattern")
+        full_pattern_list=[p_b_set[p_name][i%len(p_b_set[p_name])][:] for i in range(cnt)]
 
     if (p_name in inverted_patterns) and fix:
         full_pattern_list[0][0]=full_pattern_list[-1][-1]
